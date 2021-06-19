a141152916746363662566280414a1ca9db44f8d
treetop-fish <aaron.ty.liu@gmail.com>
2021-06-19 10:17:47 -0400

    fix typo in unet

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -24,7 +24,7 @@ class UNet(nn.Module):
         self.up3 = Up(256, 128 // factor, bilinear)
         self.up4 = Up(128, 64, bilinear)
         self.outc = OutConv(64, n_classes)
-        self.sigmoid == nn.Sigmoid()
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         x1 = self.inc(x)
