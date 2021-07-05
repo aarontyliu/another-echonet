31b81c3dab40a52288869dc17e2ca95c518fae1a
treetop-fish <aaron.ty.liu@gmail.com>
2021-07-04 22:02:24 -0400

    add mixpool

--- cspresunet/cspresunet_parts.py
+++ cspresunet/cspresunet_parts.py
@@ -5,9 +5,11 @@
    Code structure reference: https://github.com/milesial/Pytorch-UNet
 """
 
+import einops
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from einops.layers.torch import Rearrange
 
 
 class CSPLevelBlock(nn.Module):
@@ -22,15 +24,16 @@ class CSPLevelBlock(nn.Module):
     ):
         super().__init__()
         self.stride = stride
+        self.channeling = MixPooling() if stride[0] == 2 else nn.Identity()
         exp_channels = int(round(out_channels * expand_ratio))
         self.expand_layer = nn.Sequential(
             nn.BatchNorm2d(in_channels),
-            nn.ReLU(inplace=True),
+            nn.Mish(inplace=True),
             nn.Conv2d(
                 in_channels,
                 exp_channels,
                 kernel_size=1,
-                stride=self.stride[0],
+                stride=self.stride[1],
                 bias=False,
             ),
         )
@@ -38,7 +41,7 @@ class CSPLevelBlock(nn.Module):
         self.stride = stride
         self.double_conv = nn.Sequential(
             nn.BatchNorm2d(in_channels),
-            nn.ReLU(inplace=True),
+            nn.Mish(inplace=True),
             nn.Conv2d(
                 in_channels,
                 in_channels,
@@ -48,7 +51,7 @@ class CSPLevelBlock(nn.Module):
                 bias=False,
             ),
             nn.BatchNorm2d(in_channels),
-            nn.ReLU(inplace=True),
+            nn.Mish(inplace=True),
             nn.Conv2d(
                 in_channels,
                 in_channels,
@@ -61,21 +64,22 @@ class CSPLevelBlock(nn.Module):
 
         self.partial_trans2 = nn.Sequential(
             nn.BatchNorm2d(in_channels),
-            nn.ReLU(inplace=True),
+            nn.Mish(inplace=True),
             nn.Conv2d(in_channels, in_channels, 1, bias=False),
         )
 
         self.partial_trans_head = nn.Sequential(
             nn.BatchNorm2d(exp_channels),
-            nn.ReLU(inplace=True),
+            nn.Mish(inplace=True),
             nn.Conv2d(exp_channels, out_channels, 1, bias=False),
         )
 
     def forward(self, x):
+        x = self.channeling(x)
         x = self.expand_layer(x)
         half = x.size(1) // 2
         part1, part2 = x[:, :half], x[:, half:]
-        part2 = self.partial_trans2((self.double_conv(part2) + part2))
+        part2 = self.partial_trans2((self.double_conv(part2) + part2)).contiguous()
         x = self.partial_trans_head(torch.cat([part1, part2], dim=1))
 
         return x
@@ -150,7 +154,13 @@ class Up(nn.Module):
     ):
         super().__init__()
         self.stride = stride
-        self.up = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
+        self.up = nn.Sequential(
+            nn.BatchNorm2d(in_channels),
+            nn.Mish(inplace=True),
+            nn.ConvTranspose2d(
+                in_channels, out_channels, kernel_size=2, stride=2, bias=False
+            ),
+        )
         if use_csp:
             self.level_block = CSPLevelBlock(
                 in_channels, out_channels, self.stride, expand_ratio
@@ -160,10 +170,6 @@ class Up(nn.Module):
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
-        diffY = x2.size()[2] - x1.size()[2]
-        diffX = x2.size()[3] - x1.size()[3]
-        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
-
         x = torch.cat([x2, x1], dim=1)
         x = self.level_block(x)
         return x
@@ -178,8 +184,6 @@ class Stem(nn.Module):
             self.use_csp = 1
             exp_channels = int(round(out_channels * expand_ratio))
             self.expand_layer = nn.Sequential(
-                nn.BatchNorm2d(in_channels),
-                nn.ReLU(inplace=True),
                 nn.Conv2d(
                     in_channels,
                     exp_channels,
@@ -189,28 +193,30 @@ class Stem(nn.Module):
             )
             in_channels = exp_channels // 2
             self.stem_block = nn.Sequential(
+                nn.BatchNorm2d(in_channels),
+                nn.Mish(inplace=True),
                 nn.Conv2d(in_channels, in_channels, 3, padding=1, bias=False),
                 nn.BatchNorm2d(in_channels),
-                nn.ReLU(inplace=True),
+                nn.Mish(inplace=True),
                 nn.Conv2d(in_channels, in_channels, 3, padding=1, bias=False),
             )
 
             self.partial_trans2 = nn.Sequential(
                 nn.BatchNorm2d(in_channels),
-                nn.ReLU(inplace=True),
+                nn.Mish(inplace=True),
                 nn.Conv2d(in_channels, in_channels, 1, bias=False),
             )
 
             self.partial_trans_head = nn.Sequential(
                 nn.BatchNorm2d(exp_channels),
-                nn.ReLU(inplace=True),
+                nn.Mish(inplace=True),
                 nn.Conv2d(exp_channels, out_channels, 1, bias=False),
             )
         else:
             self.stem_block = nn.Sequential(
                 nn.Conv2d(in_channels, out_channels, 3, padding=1, bias=False),
                 nn.BatchNorm2d(out_channels),
-                nn.ReLU(inplace=True),
+                nn.Mish(inplace=True),
                 nn.Conv2d(out_channels, out_channels, 3, padding=1, bias=False),
             )
             self.shortcut = nn.Conv2d(in_channels, out_channels, 1, bias=False)
@@ -220,8 +226,91 @@ class Stem(nn.Module):
             x = self.expand_layer(x)
             half = x.size(1) // 2
             part1, part2 = x[:, :half], x[:, half:]
-            part2 = self.partial_trans2((self.stem_block(part2) + part2))
+            part2 = self.partial_trans2((self.stem_block(part2) + part2)).contiguous()
             x = self.partial_trans_head(torch.cat([part1, part2], dim=1))
         else:
             x = self.stem_block(x) + self.shortcut(x)
         return x
+
+
+class MixPooling(nn.Module):
+    def __init__(self, kernel_size=2):
+        super().__init__()
+        self.max_pool2d = nn.MaxPool2d(kernel_size)
+        self.avg_pool2d = nn.AvgPool2d(kernel_size)
+        self.gamma = nn.Parameter(torch.Tensor([0.5]))
+
+    def forward(self, x):
+        gamma = torch.sigmoid(self.gamma)
+        return gamma * self.max_pool2d(x) + (1 - gamma) * self.avg_pool2d(x)
+
+
+class DetailPreservegPool(nn.Module):
+    """Detailed-Preserving Pooling"""
+
+    def __init__(self, kernel_size=2, stride=None, padding=0, variant="sym"):
+        super().__init__()
+        self.filter = nn.Conv2d(
+            in_channels, in_channels, kernel_size=3, padding=1, bias=False
+        )
+        self.k = _pair(kernel_size)
+        if not stride:
+            self.stride = _pair(kernel_size)
+        else:
+            self.stride = _pair(stride)
+        self.padding = _quadruple(padding)  # convert to l, r, t, b
+
+    def _padding(self, x):
+        padding = self.padding
+        return padding
+
+    def _pho(self, x, beta=0.5, variant="sym"):
+        if variant == "sym":
+            return torch.sqrt(x ** 2 + 1e-6) ** beta
+
+    def forward(self, I):
+        I_ = self.filter(I)
+
+        x = F.pad(x, self._padding(x), mode="reflect")
+        x = x.unfold(2, self.k[0], self.stride[0]).unfold(3, self.k[1], self.stride[1])
+        x = x.contiguous().view(x.size()[:4] + (-1,)).median(dim=-1)[0]
+        return x
+
+
+# Experimenting
+class SelfAttenion(nn.Module):
+    def __init__(self, in_channels, embed_dim=None, num_heads=1):
+        super().__init__()
+        if not embed_dim:
+            embed_dim = in_channels // 32
+        self.transpose = Rearrange("b c n -> b n c")
+        self.flatten = Rearrange("b c v1 v2 -> b c (v1 v2)")
+
+        self.f = nn.Sequential(
+            nn.Conv2d(in_channels, embed_dim, 1), self.flatten, self.transpose
+        )
+        self.g = nn.Sequential(
+            nn.Conv2d(in_channels, embed_dim, 1), self.flatten, self.transpose
+        )
+        self.h = nn.Sequential(
+            nn.Conv2d(in_channels, embed_dim, 1), self.flatten, self.transpose
+        )
+        self.v = nn.Conv2d(embed_dim, in_channels, 1)
+        self.multihead_attn = nn.MultiheadAttention(
+            embed_dim, num_heads, batch_first=True
+        )
+
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+    def forward(self, x):
+        _, _, h, w = x.size()
+        query, key, value = self.f(x), self.g(x), self.h(x)
+        attn_output, _ = self.multihead_attn(query, key, value)
+        o = self.v(
+            einops.rearrange(
+                self.transpose(attn_output), "b l (h w) -> b l h w", h=h, w=w
+            )
+        )
+        output = x + self.gamma * o
+
+        return F.avg_pool2d(output, 2)
