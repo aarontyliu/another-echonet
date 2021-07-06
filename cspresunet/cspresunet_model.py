73d260d175fb0f6fbee4c44f1dd9f63b13cd2606
treetop-fish <aaron.ty.liu@gmail.com>
2021-07-06 19:54:12 -0400

    clean module

--- cspresunet/cspresunet_model.py
+++ cspresunet/cspresunet_model.py
@@ -12,37 +12,28 @@
         - Batch Normalization: https://arxiv.org/pdf/1502.03167.pdf
 """
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+from torch import nn
 
-from .cspresunet_parts import IC, CSPLevelBlock, Down, Up
+from .cspresunet_parts import Down, Stem, Up
 
 
 class CSPResUNet(nn.Module):
     def __init__(self, n_channels, n_classes, expand_ratio=1.0):
         super(CSPResUNet, self).__init__()
-        self.stem = CSPLevelBlock(n_channels, 64, expand_ratio)
+        self.stem = Stem(n_channels, 64, expand_ratio)
         self.down1 = Down(64, 128, expand_ratio)
         self.down2 = Down(128, 256, expand_ratio)
         self.down3 = Down(256, 512, expand_ratio)
-
-        # implicit data augmentation (Ronneberger et at., 2015).
-        self.up1 = Up(512, 256, expand_ratio, dropout=0.1)
+        self.up1 = Up(512, 256, expand_ratio)
         self.up2 = Up(256, 128, expand_ratio)
         self.up3 = Up(128, 64, expand_ratio)
-        self.outconv = nn.Sequential(
-            IC(64, 0.05),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, n_classes, kernel_size=1),
-        )
+        self.outconv = nn.Conv2d(64, n_classes, 1)
 
     def forward(self, x):
         x1 = self.stem(x)
         x2 = self.down1(x1)
         x3 = self.down2(x2)
         x4 = self.down3(x3)
-
         x = self.up1(x4, x3)
         x = self.up2(x, x2)
         x = self.up3(x, x1)
--- cspresunet/cspresunet_parts.py
+++ cspresunet/cspresunet_parts.py
@@ -5,29 +5,27 @@
     Code structure reference: https://github.com/milesial/Pytorch-UNet
 """
 
-import einops
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from einops.layers.torch import Rearrange, Reduce
 
 
-class CSPLevelBlock(nn.Module):
+class Stem(nn.Module):
     """
           Input (x)
              |
-         Expanding (for cross stage partial design)
+    Conv2d only expanding
+    (for cross stage partial design)
              |
         Part1, Part2
          /       |
         /        |__________________________
        /         |                          |
       /      BatchNorm2d                    |
-     /          ReLU                        |
+     /          Mish                        |
     |          Conv2d                       |
     |            |                          |
     |        BatchNorm2d              Pre-activation Shortcut
-    |           ReLU                        |
+    |           Mish                        |
     |          Conv2d                       |
     |            |                          |
     |   Squeeze and Excite (SE)             |
@@ -35,7 +33,7 @@ class CSPLevelBlock(nn.Module):
     |            |
     |        Transition
     |            |
-    |______ ______|
+    |______ _____|
            |
            |
        Transition
@@ -45,12 +43,10 @@ class CSPLevelBlock(nn.Module):
     """
 
     def __init__(self, in_channels, out_channels, expand_ratio=1.0):
-        super(CSPLevelBlock, self).__init__()
-        assert expand_ratio > 0
+        super(Stem, self).__init__()
+        assert expand_ratio > 0.0
         exp_channels = int(round(out_channels * expand_ratio))
         self.expand_layer = nn.Sequential(
-            nn.BatchNorm2d(in_channels),
-            nn.ReLU(inplace=True),
             nn.Conv2d(
                 in_channels,
                 exp_channels,
@@ -62,7 +58,7 @@ class CSPLevelBlock(nn.Module):
         self.se = SE(in_channels)
         self.double_conv = nn.Sequential(
             nn.BatchNorm2d(in_channels),
-            nn.ReLU(inplace=True),
+            nn.Mish(inplace=True),
             nn.Conv2d(
                 in_channels,
                 in_channels,
@@ -71,7 +67,7 @@ class CSPLevelBlock(nn.Module):
                 bias=False,
             ),
             nn.BatchNorm2d(in_channels),
-            nn.ReLU(inplace=True),
+            nn.Mish(inplace=True),
             nn.Conv2d(
                 in_channels,
                 in_channels,
@@ -83,13 +79,107 @@ class CSPLevelBlock(nn.Module):
 
         self.transition_pt2 = nn.Sequential(
             nn.BatchNorm2d(in_channels),
-            nn.ReLU(inplace=True),
+            nn.Mish(inplace=True),
             nn.Conv2d(in_channels, in_channels, kernel_size=1, bias=False),
         )
 
         self.transition = nn.Sequential(
             nn.BatchNorm2d(exp_channels),
-            nn.ReLU(inplace=True),
+            nn.Mish(inplace=True),
+            nn.Conv2d(exp_channels, out_channels, kernel_size=1, bias=False),
+        )
+
+    def forward(self, x):
+        x = self.expand_layer(x)
+        half = x.size(1) // 2
+        part1, part2 = x[:, :half], x[:, half:]
+        part2 = self.transition_pt2(
+            (self.se(self.double_conv(part2)) + part2)
+        ).contiguous()
+        x = self.transition(torch.cat([part1, part2], dim=1))
+
+        return x
+
+
+class CSPLevelBlock(nn.Module):
+    """
+          Input (x)
+             |
+         Expanding
+         (for cross stage partial design)
+             |
+        Part1, Part2
+         /       |
+        /        |__________________________
+       /         |                          |
+      /      BatchNorm2d                    |
+     /          Mish                        |
+    |          Conv2d                       |
+    |            |                          |
+    |        BatchNorm2d              Pre-activation Shortcut
+    |           Mish                        |
+    |          Conv2d                       |
+    |            |                          |
+    |   Squeeze and Excite (SE)             |
+    |            |__________________________|
+    |            |
+    |        Transition
+    |            |
+    |______ _____|
+           |
+           |
+       Transition
+           |
+         Output
+
+    """
+
+    def __init__(self, in_channels, out_channels, expand_ratio=1.0):
+        super(CSPLevelBlock, self).__init__()
+        assert expand_ratio > 0.0
+        exp_channels = int(round(out_channels * expand_ratio))
+        self.expand_layer = nn.Sequential(
+            nn.BatchNorm2d(in_channels),
+            nn.Mish(inplace=True),
+            nn.Conv2d(
+                in_channels,
+                exp_channels,
+                kernel_size=1,
+                bias=False,
+            ),
+        )
+        in_channels = exp_channels // 2
+        self.se = SE(in_channels)
+        self.double_conv = nn.Sequential(
+            nn.BatchNorm2d(in_channels),
+            nn.Mish(inplace=True),
+            nn.Conv2d(
+                in_channels,
+                in_channels,
+                kernel_size=3,
+                padding=1,
+                bias=False,
+            ),
+            nn.BatchNorm2d(in_channels),
+            nn.Mish(inplace=True),
+            nn.Conv2d(
+                in_channels,
+                in_channels,
+                kernel_size=3,
+                padding=1,
+                bias=False,
+            ),
+        )
+
+        self.transition_pt2 = nn.Sequential(
+            nn.BatchNorm2d(in_channels),
+            nn.Mish(inplace=True),
+            nn.Conv2d(in_channels, in_channels, kernel_size=1, bias=False),
+        )
+
+        self.transition = nn.Sequential(
+            nn.BatchNorm2d(exp_channels),
+            nn.Mish(inplace=True),
             nn.Conv2d(exp_channels, out_channels, kernel_size=1, bias=False),
         )
 
@@ -118,11 +208,11 @@ class Down(nn.Module):
 
 
 class Up(nn.Module):
-    def __init__(self, in_channels, out_channels, expand_ratio=1.0, dropout=0):
+    def __init__(self, in_channels, out_channels, expand_ratio=1.0):
         super(Up, self).__init__()
         self.up = nn.Sequential(
-            IC(in_channels, dropout),
-            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(in_channels),
+            nn.Mish(inplace=True),
             nn.ConvTranspose2d(
                 in_channels, out_channels, kernel_size=2, stride=2, bias=False
             ),
@@ -157,7 +247,7 @@ class SE(nn.Module):
         self.squeeze = nn.AdaptiveAvgPool2d(1)
         self.reduce_expand = nn.Sequential(
             nn.Conv2d(in_channels, num_squeezed_channels, 1),
-            nn.ReLU(inplace=True),
+            nn.Mish(inplace=True),
             nn.Conv2d(num_squeezed_channels, in_channels, 1),
             nn.Sigmoid(),
         )
@@ -167,55 +257,3 @@ class SE(nn.Module):
         variances = self.squeeze((x - means) ** 2)
         attn = self.reduce_expand(means * variances)
         return x * attn
-
-
-class IC(nn.Module):
-    """Independent component"""
-
-    def __init__(self, in_channels, p=0.05):
-        super(IC, self).__init__()
-        self.ic = nn.Sequential(
-            nn.BatchNorm2d(in_channels), nn.Dropout(p, inplace=True)
-        )
-
-    def forward(self, x):
-        return self.ic(x)
-
-
-# Experimenting
-class SelfAttenion(nn.Module):
-    def __init__(self, in_channels, embed_dim=None, num_heads=1):
-        super().__init__()
-        if not embed_dim:
-            embed_dim = in_channels // 32
-        self.transpose = Rearrange("b c n -> b n c")
-        self.flatten = Rearrange("b c v1 v2 -> b c (v1 v2)")
-
-        self.f = nn.Sequential(
-            nn.Conv2d(in_channels, embed_dim, 1), self.flatten, self.transpose
-        )
-        self.g = nn.Sequential(
-            nn.Conv2d(in_channels, embed_dim, 1), self.flatten, self.transpose
-        )
-        self.h = nn.Sequential(
-            nn.Conv2d(in_channels, embed_dim, 1), self.flatten, self.transpose
-        )
-        self.v = nn.Conv2d(embed_dim, in_channels, 1)
-        self.multihead_attn = nn.MultiheadAttention(
-            embed_dim, num_heads, batch_first=True
-        )
-
-        self.gamma = nn.Parameter(torch.zeros(1))
-
-    def forward(self, x):
-        _, _, h, w = x.size()
-        query, key, value = self.f(x), self.g(x), self.h(x)
-        attn_output, _ = self.multihead_attn(query, key, value)
-        o = self.v(
-            einops.rearrange(
-                self.transpose(attn_output), "b l (h w) -> b l h w", h=h, w=w
-            )
-        )
-        output = x + self.gamma * o
-
-        return F.avg_pool2d(output, 2)
